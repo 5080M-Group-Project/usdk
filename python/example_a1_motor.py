cd6686b4c96ebeb0827d3bfc3a0bfbdad2ae8ac5
Kevin <a048598@gmail.com>
2025-03-12 20:25:19 +0800

    fix get_motor_encoder

--- python/example_a1_motor.py
+++ python/example_a1_motor.py
@@ -8,16 +8,16 @@ serial = SerialPort('/dev/ttyUSB0')
 cmd = MotorCmd()
 data = MotorData()
 
-print("Add test")
+
 while True:
     data.motorType = MotorType.A1
     cmd.motorType = MotorType.A1
     cmd.mode = queryMotorMode(MotorType.A1,MotorMode.FOC)
     cmd.id   = 0
     cmd.q    = 0.0
-    cmd.dq   = 6.28*queryGearRatio(MotorType.A1)
+    cmd.dq   = 0*queryGearRatio(MotorType.A1)
     cmd.kp   = 0.0
-    cmd.kd   = 2
+    cmd.kd   = 0
     cmd.tau  = 0.0
     serial.sendRecv(cmd, data)
     print('\n')
@@ -26,5 +26,5 @@ while True:
     print("temp: " + str(data.temp))
     print("merror: " + str(data.merror))
     print('\n')
-    time.sleep(0.0002) # 200 us
+    time.sleep(0.0002) # 200 us  
 

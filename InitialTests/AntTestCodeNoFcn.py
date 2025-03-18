480cab2d15fa25e65b3c997c066a3e67e9d18e2d
pm20ag <pm20ag@leeds.ac.uk>
2025-03-18 14:25:00 +0000

    Command & Data syntax updated for python

--- InitialTests/AntTestCodeNoFcn.py
+++ InitialTests/AntTestCodeNoFcn.py
@@ -9,8 +9,6 @@ cmd = MotorCmd()
 data = MotorData()
 gearRatio = queryGearRatio(MotorType.A1)
 
-
-
 # Initialize Hip Motor
 kpOutHip, kdOutHip, kpRotorHip, kdRotorHip = 25, 0.6, 0.0, 0.0
 kpRotorHip = (kpOutHip / (gearRatio * gearRatio)) / 26.07
@@ -18,13 +16,14 @@ kdRotorHip = (kdOutHip / (gearRatio * gearRatio)) / 100.0
 data.motorType = MotorType.A1
 cmd.motorType = MotorType.A1
 cmd.mode = queryMotorMode(MotorType.A1, MotorMode.FOC)
-cmd.id    = 0;
-cmd.kp    = 0.0; #proportional or position term. i.e. stiffness
-cmd.kd    = 0.0; #derivative or velocity term, i.e damping
-cmd.q     = 0.0; #angle, radians
-cmd.dq    = 0.0; #angular velocity, radians/s
-cmd.tau   = 0.0; #rotor feedforward torque
+cmd.id    = 0
+cmd.kp    = 0.0 #proportional or position term. i.e. stiffness
+cmd.kd    = 0.0 #derivative or velocity term, i.e damping
+cmd.q     = 0.0 #angle, radians
+cmd.dq    = 0.0 #angular velocity, radians/s
+cmd.tau   = 0.0 #rotor feedforward torque
 serial.sendRecv(cmd, data)
+
 hipAngleOutputInitial = (data.q / gearRatio) * (180 / np.pi)
 
 # Initialize Knee Motor
@@ -35,13 +34,14 @@ kdRotorKnee = (kdOutKnee / (gearRatio * gearRatio)) / 100.0
 data.motorType = MotorType.A1
 cmd.motorType = MotorType.A1
 cmd.mode = queryMotorMode(MotorType.A1, MotorMode.FOC)
-cmd.id    = 1;
-cmd.kp    = 0.0; #proportional or position term. i.e. stiffness
-cmd.kd    = 0.0; #derivative or velocity term, i.e damping
-cmd.q     = 0.0; #angle, radians
-cmd.dq    = 0.0; #angular velocity, radians/s
-cmd.tau   = 0.0; #rotor feedforward torque
+cmd.id    = 1
+cmd.kp    = 0.0 #proportional or position term. i.e. stiffness
+cmd.kd    = 0.0 #derivative or velocity term, i.e damping
+cmd.q     = 0.0 #angle, radians
+cmd.dq    = 0.0 #angular velocity, radians/s
+cmd.tau   = 0.0 #rotor feedforward torque
 serial.sendRecv(cmd, data)
+
 kneeAngleOutputInitial = (data.q / gearRatio) * (180 / np.pi)
 
 # Initialize Wheel Motor
@@ -52,13 +52,14 @@ kdRotorWheel = (kdOutWheel / (gearRatio * gearRatio)) / 100.0
 data.motorType = MotorType.A1
 cmd.motorType = MotorType.A1
 cmd.mode = queryMotorMode(MotorType.A1, MotorMode.FOC)
-cmd.id    = 2;
-cmd.kp    = 0.0; #proportional or position term. i.e. stiffness
-cmd.kd    = 0.0; #derivative or velocity term, i.e damping
-cmd.q     = 0.0; #angle, radians
-cmd.dq    = 0.0; #angular velocity, radians/s
-cmd.tau   = 0.0; #rotor feedforward torque
+cmd.id    = 2
+cmd.kp    = 0.0 #proportional or position term. i.e. stiffness
+cmd.kd    = 0.0 #derivative or velocity term, i.e damping
+cmd.q     = 0.0 #angle, radians
+cmd.dq    = 0.0 #angular velocity, radians/s
+cmd.tau   = 0.0 #rotor feedforward torque
 serial.sendRecv(cmd, data)
+
 wheeAngleOutputInitial = (data.q / gearRatio) * (180 / np.pi)
 
 
@@ -76,65 +77,65 @@ while True:
         data.motorType = MotorType.A1
         cmd.motorType = MotorType.A1
         cmd.mode = queryMotorMode(MotorType.A1, MotorMode.FOC)
-        cmd.id    = 0;
-        cmd.kp    = kpRotorHip; #proportional or position term. i.e. stiffness
-        cmd.kd    = kdRotorHip; #derivative or velocity term, i.e damping
-        cmd.q     = hipRotorAngleDesired; #angle, radians
-        cmd.dq    = 0.0; #angular velocity, radians/s
-        cmd.tau   = hipTau; #rotor feedforward torque
+        cmd.id    = 0
+        cmd.kp    = kpRotorHip #proportional or position term. i.e. stiffness
+        cmd.kd    = kdRotorHip #derivative or velocity term, i.e damping
+        cmd.q     = hipRotorAngleDesired #angle, radians
+        cmd.dq    = 0.0 #angular velocity, radians/s
+        cmd.tau   = hipTau #rotor feedforward torque
         serial.sendRecv(cmd, data)
-        torque = hipTau + kpRotorHip * (hipRotorAngleDesired - data.q) + kdRotorHip * (0.0 - data.dq);
-        std::cout << std::endl;
-        std::cout <<"Hip Motor" << std::endl;
-        std::cout << "Angle (rad): " << data.q / gearRatio << std::endl;
-        std::cout << "Angular Velocity (rad/s): " << data.dq / gearRatio << std::endl;
-        std::cout << "Torque (N.m): " << torque << std::endl;
-        std::cout << "Temperature: " << data.temp << std::endl;
-        std::cout << "ISSUE? " << data.merror << std::endl;
-        std::cout << std::endl;
+        torque = hipTau + kpRotorHip * (hipRotorAngleDesired - data.q) + kdRotorHip * (0.0 - data.dq)
+        print('\n')
+        print("Hip Motor")
+        print("Angle (rad): " + str(data.q / gearRatio))
+        print("Angular Velocity (rad/s): " + str(data.dq / gearRatio))
+        print("Torque (N.m): " + str(torque))
+        print("Temperature: " + str(data.temp))
+        print("ERROR?: " + str(data.merror))
+        print('\n')
 
         # Knee Motor Control
         ######DETERMINING DESIRED ANGLE#######
         data.motorType = MotorType.A1
         cmd.motorType = MotorType.A1
         cmd.mode = queryMotorMode(MotorType.A1, MotorMode.FOC)
-        cmd.id    = 1;
-        cmd.kp    = kpRotorKnee; #proportional or position term. i.e. stiffness
-        cmd.kd    = kdRotorKnee; #derivative or velocity term, i.e damping
-        cmd.q     = kneeRotorAngleDesired; #angle, radians
-        cmd.dq    = 0.0; #angular velocity, radians/s
-        cmd.tau   = kneeTau; #rotor feedforward torque
+        cmd.id    = 1
+        cmd.kp    = kpRotorKnee #proportional or position term. i.e. stiffness
+        cmd.kd    = kdRotorKnee #derivative or velocity term, i.e damping
+        cmd.q     = kneeRotorAngleDesired #angle, radians
+        cmd.dq    = 0.0 #angular velocity, radians/s
+        cmd.tau   = kneeTau #rotor feedforward torque
         serial.sendRecv(cmd, data)
         torque = kneeTau + kpRotorKnee * (KneeRotorAngleDesired - data.q) + kdRotorKnee * (0.0 - data.dq);
-        std::cout << std::endl;
-        std::cout << "Knee Motor" << std::endl;
-        std::cout << "Angle (rad): " << data.q / gearRatio << std::endl;
-        std::cout << "Angular Velocity (rad/s): " << data.dq / gearRatio << std::endl;
-        std::cout << "Torque (N.m): " << torque << std::endl;
-        std::cout << "Temperature: " << data.temp << std::endl;
-        std::cout << "ISSUE? " << data.merror << std::endl;
-        std::cout << std::endl;
+        print('\n')
+        print("Knee Motor")
+        print("Angle (rad): " + str(data.q / gearRatio))
+        print("Angular Velocity (rad/s): " + str(data.dq / gearRatio))
+        print("Torque (N.m): " + str(torque))
+        print("Temperature: " + str(data.temp))
+        print("ERROR?: " + str(data.merror))
+        print('\n')
 
         # Wheel Motor Control
         ######DETERMINING DESIRED ANGULAR VELOCITY#######
         data.motorType = MotorType.A1
         cmd.motorType = MotorType.A1
         cmd.mode = queryMotorMode(MotorType.A1, MotorMode.FOC)
-        cmd.id    = 2;
-        cmd.kp    = 0.0; #proportional or position term. i.e. stiffness
-        cmd.kd    = kdRotorWheel; #derivative or velocity term, i.e damping
-        cmd.q     = 0.0; #angle, radians
-        cmd.dq    = wheelRotorAngularVelocityDesired; #angular velocity, radians/s
-        cmd.tau   = 0.0; #rotor feedforward torque
+        cmd.id    = 2
+        cmd.kp    = 0.0 #proportional or position term. i.e. stiffness
+        cmd.kd    = kdRotorWheel #derivative or velocity term, i.e damping
+        cmd.q     = 0.0 #angle, radians
+        cmd.dq    = wheelRotorAngularVelocityDesired #angular velocity, radians/s
+        cmd.tau   = 0.0 #rotor feedforward torque
         serial.sendRecv(cmd, data)
         torque = 0.0 + 0.0 * (0.0 - data.q) + kdRotorWheel * (wheelRotorAngularVelocityDesired - data.dq);
-        std::cout << std::endl;
-        std::cout << "Wheel Motor" << std::endl;
-        std::cout << "Angle (rad): " << data.q / gearRatio << std::endl;
-        std::cout << "Angular Velocity (rad/s): " << data.dq / gearRatio << std::endl;
-        std::cout << "Torque (N.m): " << torque << std::endl;
-        std::cout << "Temperature: " << data.temp << std::endl;
-        std::cout << "ISSUE? " << data.merror << std::endl;
-        std::cout << std::endl;
+        print('\n')
+        print("Wheel Motor")
+        print("Angle (rad): " + str(data.q / gearRatio))
+        print("Angular Velocity (rad/s): " + str(data.dq / gearRatio))
+        print("Torque (N.m): " + str(torque))
+        print("Temperature: " + str(data.temp))
+        print("ERROR?: " + str(data.merror))
+        print('\n')
 
         time.sleep(0.0002) # 200 us

f10a2f8576bfc399fc41b637c96a235af57234a5
pm20ag <pm20ag@leeds.ac.uk>
2025-03-28 12:54:32 +0000

    Update AngleReadingTest.py

--- Test/AngleReadingTest.py
+++ Test/AngleReadingTest.py
@@ -27,48 +27,63 @@ serial.sendRecv(cmd, data)
 kneeInitial = ((data.q /queryGearRatio(MotorType.A1)) * (180 / np.pi))
 offsetKnee  = 0.0 - kneeInitial
 
+calibration = False
 
 while True:
-    data.motorType = MotorType.A1
-    cmd.motorType = MotorType.A1
-    cmd.mode = queryMotorMode(MotorType.A1,MotorMode.FOC)
-    cmd.id   = 0
-    cmd.q    = 0.0
-    cmd.dq   = 0.0 #6.28*queryGearRatio(MotorType.A1)
-    cmd.kp   = 0.0
-    cmd.kd   = 0.0
-    cmd.tau  = 0.0
-    serial.sendRecv(cmd, data)
+    if calibration == False:
+        data.motorType = MotorType.A1
+        cmd.id = 0
+        serial.sendRecv(cmd, data)
+        hipInitial = ((data.q / queryGearRatio(MotorType.A1)) * (180 / np.pi))
+        offsetHip = 90.0 - hipInitial
 
-    Angle = ((data.q / queryGearRatio(MotorType.A1)) * (180 / np.pi)) + offsetHip
+        data.motorType = MotorType.A1
+        cmd.id = 1
+        serial.sendRecv(cmd, data)
+        kneeInitial = ((data.q / queryGearRatio(MotorType.A1)) * (180 / np.pi))
+        offsetKnee = 0.0 - kneeInitial
+        calibration = True
+    else:
+        data.motorType = MotorType.A1
+        cmd.motorType = MotorType.A1
+        cmd.mode = queryMotorMode(MotorType.A1, MotorMode.FOC)
+        cmd.id = 0
+        cmd.q = 0.0
+        cmd.dq = 0.0  # 6.28*queryGearRatio(MotorType.A1)
+        cmd.kp = 0.0
+        cmd.kd = 0.0
+        cmd.tau = 0.0
+        serial.sendRecv(cmd, data)
 
-    print('\n')
-    print("Raw Output Angle (Hip) rad: " + str((data.q / queryGearRatio(MotorType.A1))))
-    print("Raw Output Angle (Hip) deg: " + str((data.q / queryGearRatio(MotorType.A1))*(180 / np.pi)))
-    print("Angle w. offset (Hip) deg: " + str(Angle))
-    print("Initial (Hip) deg: " + str(hipInitial))
-    print("Offset (Hip) deg: " + str(offsetHip))
-    print('\n')
-    time.sleep(0.0002) # 200 us
+        Angle = ((data.q / queryGearRatio(MotorType.A1)) * (180 / np.pi)) + offsetHip
 
-    data.motorType = MotorType.A1
-    cmd.motorType = MotorType.A1
-    cmd.mode = queryMotorMode(MotorType.A1,MotorMode.FOC)
-    cmd.id   = 1
-    cmd.q    = 0.0
-    cmd.dq   = 0.0 #6.28*queryGearRatio(MotorType.A1)
-    cmd.kp   = 0.0
-    cmd.kd   = 0.0
-    cmd.tau  = 0.0
-    serial.sendRecv(cmd, data)
+        print('\n')
+        print("Raw Output Angle (Hip) rad: " + str((data.q / queryGearRatio(MotorType.A1))))
+        print("Raw Output Angle (Hip) deg: " + str((data.q / queryGearRatio(MotorType.A1)) * (180 / np.pi)))
+        print("Angle w. offset (Hip) deg: " + str(Angle))
+        print("Initial (Hip) deg: " + str(hipInitial))
+        print("Offset (Hip) deg: " + str(offsetHip))
+        print('\n')
+        time.sleep(0.0002)  # 200 us
 
-    Angle = ((data.q / queryGearRatio(MotorType.A1)) * (180 / np.pi)) + offsetKnee
+        data.motorType = MotorType.A1
+        cmd.motorType = MotorType.A1
+        cmd.mode = queryMotorMode(MotorType.A1, MotorMode.FOC)
+        cmd.id = 1
+        cmd.q = 0.0
+        cmd.dq = 0.0  # 6.28*queryGearRatio(MotorType.A1)
+        cmd.kp = 0.0
+        cmd.kd = 0.0
+        cmd.tau = 0.0
+        serial.sendRecv(cmd, data)
 
-    print('\n')
-    print("Raw Output Angle (Knee) rad: " + str((data.q / queryGearRatio(MotorType.A1))))
-    print("Raw Output Angle (Knee) deg: " + str((data.q / queryGearRatio(MotorType.A1))*(180 / np.pi)))
-    print("Angle w. offset (Knee) deg: " + str(Angle))
-    print("Initial (Knee) deg: " + str(kneeInitial))
-    print("Offset (Knee) deg: " + str(offsetKnee))
-    print('\n')
-    time.sleep(0.0002) # 200 us
+        Angle = ((data.q / queryGearRatio(MotorType.A1)) * (180 / np.pi)) + offsetKnee
+
+        print('\n')
+        print("Raw Output Angle (Knee) rad: " + str((data.q / queryGearRatio(MotorType.A1))))
+        print("Raw Output Angle (Knee) deg: " + str((data.q / queryGearRatio(MotorType.A1)) * (180 / np.pi)))
+        print("Angle w. offset (Knee) deg: " + str(Angle))
+        print("Initial (Knee) deg: " + str(kneeInitial))
+        print("Offset (Knee) deg: " + str(offsetKnee))
+        print('\n')
+        time.sleep(0.0002)  # 200 us

28b92aa92b15b6b95e4f2d5e4dbaf0a411d985ec
sergioleeds <el20slr@leeds.ac.uk>
2025-04-04 13:28:30 +0100

    changeID code fixed

--- example/changeID.cpp
+++ example/changeID.cpp
@@ -1,51 +1,52 @@
-#include <stdio.h>      // Standard input/output functions
-#include <string.h>     // String handling functions
-#include <unistd.h>     // UNIX system functions (usleep for sleep)
-#include <stdlib.h>     // Standard library functions
-#include "serialPort/SerialPort.h"  // Serial communication
-#include "unitreeMotor/unitreeMotor.h"  // Unitree motor commands
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "serialPort/SerialPort.h"
+#include "unitreeMotor/unitreeMotor.h"
 
-#define BroadAllMotorID     0xBB  // Special ID to broadcast to all motors
-#define MotorPulsator       11    // Mode to enter "pulsator mode" (for changing motor ID)
+#define BroadAllMotorID     0xBB
+#define MotorPulsator       11
 
 int main(){
-    char serial_name[100];  // Stores the serial port name
+    char serial_name[100];
 
-    MotorCmd cmd;  // Stores motor command data
-    MotorData data;  // Stores received motor data
+    MotorCmd motor_s;
+    MotorData motor_r;
 
-    cmd.motorType = MotorType::B1;    // Set the motor type, either A1 or B1
-    data.motorType = cmd.motorType;
+    motor_s.motorType = MotorType::B1;    // set the motor type, A1 or B1
+    motor_r.motorType = motor_s.motorType;
 
-    printf("Please input the name of the serial port. (e.g., Linux:/dev/ttyUSB0, Windows:\\\\.\\COM3)\n");
+    printf("Please input the name of serial port.(e.g. Linux:/dev/ttyUSB0, Windows:\\\\.\\COM3)\n");
     scanf("%s",serial_name);
     printf("The serial port is %s\n", serial_name);
 
-    SerialPort serial(serial_name);  // Set the serial port name
+    SerialPort serial(serial_name);  // set the serial port name
 
-    cmd.id = BroadAllMotorID;  // Send command to all motors
-    cmd.mode = 10;  // Set mode to 10 (possibly initialization or reset)
-    serial.sendRecv(&cmd, &data);  // Send command and receive data
+    motor_s.id = BroadAllMotorID;
+    motor_s.mode = 10;
+    serial.sendRecv(&motor_s, &motor_r);
+    
+    usleep(100000);  //sleep 0.1s
 
-    usleep(100000);  // Sleep for 0.1 seconds
-
-    // Enter pulsator mode (modify motor ID)
-    cmd.mode = MotorPulsator;
-    cmd.modify_data(&cmd);  // Modify the command data
-    serial.send(cmd.get_cmdend_data(), cmd.hex_len);  // Send the modified command
+    //进入拨轮模式（修改ID）
+    motor_s.mode = MotorPulsator;
+    motor_s.modify_data(&motor_s);
+    serial.send(motor_s.get_motor_send_data(), motor_s.hex_len);
 
     printf("Please turn the motor.\n");
-    printf("One turn: id=0; Two turns: id=1, Three turns: id=2\n");
-    printf("ID can only be 0, 1, or 2\n");
+    printf("One time: id=0; Two times: id=1, Three times: id=2\n");
+    printf("ID can only be 0, 1, 2\n");
     printf("Once finished, press 'a'\n");
 
-    while(getchar() != (int)'a');  // Wait for user to press 'a'
+    // int c;
+    while(getchar() != (int)'a');
     printf("Turn finished\n");
 
-    // Save the motor ID
-    cmd.mode = 0;  // Set mode to 0 (possibly saving changes)
-    cmd.modify_data(&cmd);  // Modify the command data
-    serial.send(cmd.get_cmdend_data(), cmd.hex_len);  // Send the command
+    //保存ID
+    motor_s.mode = 0;
+    motor_s.modify_data(&motor_s);
+    serial.send(motor_s.get_motor_send_data(), motor_s.hex_len);
 
     return 0;
 }

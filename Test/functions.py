df4f38e1028b67d9b97734caeb278332890e64eb
pm20ag <pm20ag@leeds.ac.uk>
2025-04-07 21:04:23 +0100

    .

--- Test/functions.py
+++ Test/functions.py
@@ -219,8 +219,7 @@ def crouchingMotionV1(crouchHeightDesired,hipOutputAngleCurrent,kneeOutputAngleC
 
 
 def getLinearInterpolationAngle(startAngle, desiredAngle, T, t):
-        currentAngle = (desiredAngle - startAngle) * t/T + startAngle
-        #currentAngle = desiredAngle*t/T + startAngle*(1 - t/T)
+        currentAngle = desiredAngle*t/T + startAngle*(1 - t/T)
         return currentAngle
 
 

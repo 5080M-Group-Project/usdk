df0bafbebda628e20ed7714d7a7c6e1aba133f4f
pm20ag <pm20ag@leeds.ac.uk>
2025-04-07 19:49:52 +0100

    Update crouchTestV2.py

--- Test/crouchTestV2.py
+++ Test/crouchTestV2.py
@@ -126,8 +126,7 @@ try:
                 else:
                         hipOutputAngleCurrent = hipOutputAngleCurrent
                         hipCommsFail =+ 1
-                        print(f"[WARNING] Hip Motor (ID {id.hip}) lost response {hipCommsFail} times! {hipCommsFail/(hipCommsFail+hipCommsSuccess)}% of attempts.")
-
+                        print(f"[WARNING] Hip Motor (ID {id.hip}) lost response {hipCommsFail} times! "  f"{100 * hipCommsFail / (hipCommsFail + hipCommsSuccess):.2f}% failure rate.")
 
                 hipTorque = calculateOutputTorque(kpRotorHip, kdRotorHip, hipRotorAngleDesired,0.0, hipTau, data.q, data.dq) #kpRotor or kpOutput??
                 outputData(id.hip,hipOutputAngleCurrent,data.dq,torque,data.temp,data.merror)
@@ -157,8 +156,7 @@ try:
                 else:
                         kneeOutputAngleCurrent = kneeOutputAngleCurrent
                         kneeCommsFail += 1
-                        print(f"[WARNING] Hip Motor (ID {id.knee}) lost response {kneeCommsFail} times! {kneeCommsFail/(kneeCommsFail+kneeCommsSuccess)}% of attempts.")
-
+                        print(f"[WARNING] Knee Motor (ID {id.knee}) lost response {kneeCommsFail} times! " f"{100 * kneeCommsFail / (kneeCommsFail + kneeCommsSuccess):.2f}% failure rate.")
 
                 kneeTorque = calculateOutputTorque(kpRotorKnee, kdRotorKnee, kneeRotorAngleDesired,0.0, kneeTau, data.q, data.dq) #kpRotor or kpOutput??
                 outputData(id.knee, kneeOutputAngleCurrent, data.dq, torque, data.temp, data.merror)

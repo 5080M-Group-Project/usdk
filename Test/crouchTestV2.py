3e3ce6f684cfa7e5ee2be73b16e6dc37ff351355
pm20ag <pm20ag@leeds.ac.uk>
2025-04-07 19:00:46 +0100

    Revert "."
    This reverts commit 63316e7921ee83b3e15f88a46f9f22eda0bca12d.
    
    # Conflicts:
    #	Test/functions.py

--- Test/crouchTestV2.py
+++ Test/crouchTestV2.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from functions import *
 
+######## NEEDED??? ########
 sys.path.append('../lib')
 from unitree_actuator_sdk import *
 
@@ -18,22 +19,13 @@ gearRatio = queryGearRatio(MotorType.A1)
 ##### NOTE 1: All rotor angles in RAD, all output angles in DEG########
 ##### NOTE 2: Whenever reading angles +offset, whenever commanding -offset. Offset in DEG######
 
-# Initialize Motor Gains - add to .h file or equivalent
+# Initialize Hip Motor
+kpOutHip, kdOutHip = 10.0, 3.0 ### IDEA: Modify throughout the loop i.e. when locking legs
+kpRotorHip, kdRotorHip = getRotorGains(kpOutHip, kdOutHip)
 
-# HIP
-kpOutHipFixed, kdOutHipFixed = 10.0, 0.5 ### IDEA: Modify throughout the loop i.e. when locking legs
-kpRotorHipFixed, kdRotorHipFixed = getRotorGains(kpOutHipFixed, kdOutHipFixed)
-
-kpOutHipMoving, kdOutHipMoving = 5.0, 3.0 ### IDEA: Modify throughout the loop i.e. when locking legs
-kpRotorHipMoving, kdRotorHipMoving = getRotorGains(kpOutHipMoving, kdOutHipMoving)
-
-# KNEE
-kpOutKneeFixed, kdOutKneeFixed = 10.0, 0.5
-kpRotorKneeFixed, kdRotorKneeFixed = getRotorGains(kpOutKneeFixed, kdOutKneeFixed)
-
-kpOutKneeMoving, kdOutKneeMoving = 5.0, 3.0
-kpRotorKneeMoving, kdRotorKneeMoving = getRotorGains(kpOutKneeMoving, kdOutKneeMoving)
-
+# Initialize Knee Motor
+kpOutKnee, kdOutKnee = 10.0, 3.0
+kpRotorKnee, kdRotorKnee = getRotorGains(kpOutKnee, kdOutKnee)
 
 '''
 cmd.motorType = MotorType.A1
@@ -55,7 +47,6 @@ cmdActuator(id.knee,0.0,0.0,0.0,0.0,0.0)
 serial.sendRecv(cmd, data)
 '''
 
-
 '''
 # Initialize Wheel Motor
 kpOutWheel, kdOutWheel = 0.0, 2.0
@@ -69,9 +60,7 @@ cmdActuator(id.wheel,0.0,0.0,0.0,0.0,0.0)
 #Initialize Loop Variables
 torque = 0.0
 hipOutputAngleDesired, kneeOutputAngleDesired = 0.0, 0.0
-kpRotorHip, kdRotorHip = 0.0, 0.0
-kpRotorKnee, kdRotorKnee = 0.0, 0.0
-wheelOutputAngularVelocityDesired = 0.0
+wheelOutputAngularVelocityDesired, wheelRotorAngularVelocityDesired = 0.0, 0.0
 hipTau, kneeTau, wheelTau = 0.0, 0.0, 0.0
 hipOffset, kneeOffset = 0.0, 0.0
 
@@ -82,7 +71,7 @@ sleepTime = 0.1
 
 crouching = False
 crouchHeightDesiredPrev = 0.33
-crouchDuration = 2.0
+crouchTime = 2.0
 
 # Data storage for plotting
 hipOutputAngles = []
@@ -134,7 +123,8 @@ try:
                 hipTorque = calculateOutputTorque(kpRotorHip, kdRotorHip, hipRotorAngleDesired,0.0, hipTau, data.q, data.dq) #kpRotor or kpOutput??
                 outputData(id.hip,hipOutputAngleCurrent,data.dq,torque,data.temp,data.merror)
 
-                hipOutputAngles.append(hipOutputAngleCurrent), hipCommandAngles.append(hipOutputAngleDesired)
+                hipOutputAngles.append(hipOutputAngleCurrent)
+                hipCommandAngles.append(hipOutputAngleDesired)
 
 
                 time.sleep(sleepTime/100.0)
@@ -161,39 +151,28 @@ try:
                 kneeTorque = calculateOutputTorque(kpRotorKnee, kdRotorKnee, kneeRotorAngleDesired,0.0, kneeTau, data.q, data.dq) #kpRotor or kpOutput??
                 outputData(id.knee, kneeOutputAngleCurrent, data.dq, torque, data.temp, data.merror)
 
-                kneeOutputAngles.append(kneeOutputAngleCurrent), kneeCommandAngles.append(kneeOutputAngleDesired)
+                kneeOutputAngles.append(kneeOutputAngleCurrent)
+                kneeCommandAngles.append(kneeOutputAngleDesired)
 
                 # Crouch Control
-                crouchTimingBegin = time.time()
-
                 crouchHeightDesiredNew = 0.2  ## max = 0.33 / ### IDEA: in future, read signal from RC controller to change
-                xWheel, crouchHeightCurrent = forwardKinematicsDeg(hipOutputAngleCurrent, kneeOutputAngleCurrent)
-                crouchThreshold = (10/100)*0.33
-                stopCrouching = abs(crouchHeightDesiredNew - crouchHeightCurrent) < crouchThreshold
-
                 #hipOutputAngleDesired, kneeOutputAngleDesired = crouchingMotion2(crouchHeightDesired,hipOutputAngleCurrent,kneeOutputAngleCurrent,sleepTime*2, 2.0)
 
                 if (crouchHeightDesiredNew != crouchHeightDesiredPrev) and not crouching:
+                        N = int(crouchTime / sleepTime)  # Ensure N is an integer
                         # Get current and desired joint angles
                         hipCrouchAngleDesired, kneeCrouchAngleDesired = inverseKinematicsDeg(0.0, -crouchHeightDesiredNew, 'front')
-                        hipCrouchAngleStart, kneeCrouchAngleStart = hipOutputAngleCurrent, kneeOutputAngleCurrent
-                        crouchStartTime = time.time()
+                        # Generate interpolation vectors
+                        thetaHipVector = np.linspace(hipOutputAngleCurrent, hipCrouchAngleDesired, num=N)
+                        thetaKneeVector = np.linspace(kneeOutputAngleDesired, kneeCrouchAngleDesired, num=N)
+                        count = 0
                         crouching = True  # Enable crouching phase
-                        stopCrouching = False #NEEDED?
-
-                elif crouching and not stopCrouching:
-
-                        hipOutputAngleDesired = getLinearInterpolationAngle(hipCrouchAngleStart, hipCrouchAngleDesired, crouchDuration, crouchStartTime - time.time())
-                        kneeOutputAngleDesired = getLinearInterpolationAngle(kneeCrouchAngleStart, kneeCrouchAngleDesired, crouchDuration, crouchStartTime - time.time())
-
-                        #####
-                        kpRotorHip, kdRotorHip = kpRotorHipMoving, kdRotorHipMoving
-                        kdRotorHip, kdRotorKnee = kpRotorKneeMoving, kdRotorKneeMoving
-                        #####
-
-                        print(f"\nAdjusting Crouch Height - Current: {crouchHeightCurrent:.3f}, Desired: {crouchHeightDesiredNew:.3f}")
-
-                elif crouching and stopCrouching:
+                elif crouching and count < len(thetaHipVector):
+                        hipOutputAngleDesired, kneeOutputAngleDesired = thetaHipVector[count], thetaKneeVector[count]
+                        count += 1  # Increment step
+                        print(f"\nCount: {(count)}\n")
+                        # print(f"\nAdjusting Crouch Height - Current: {crouchHeightCurrent:.3f}, Desired: {crouchHeightDesired:.3f}")
+                elif crouching and count >= len(thetaHipVector):
                         hipOutputAngleDesired, kneeOutputAngleDesired = hipCrouchAngleDesired, kneeCrouchAngleDesired
                         crouching = False
                         crouchHeightDesiredPrev = crouchHeightDesiredNew
@@ -201,30 +180,17 @@ try:
                 else:
                         hipOutputAngleDesired, kneeOutputAngleDesired = hipCrouchAngleDesired, kneeCrouchAngleDesired
                         crouchHeightDesiredPrev = crouchHeightDesiredNew
-
-                        #####
-                        kpRotorHip, kdRotorHip = kpRotorHipFixed, kdRotorHipFixed
-                        kdRotorHip, kdRotorKnee = kpRotorKneeFixed, kdRotorKneeFixed
-                        #####
-
                         print("\nCorrect crouch height. Legs Fixed\n")
-
 
                 loopTime = time.time() - startTime
                 print(f"Loop Time: {loopTime}\n")
-
-                crouchTimingLength = time.time() - crouchTimingBegin
-                print(f"Crouch Time: {crouchTimingLength}\n")
-
-                preCrouchTimingLength =  crouchTimingBegin - startTime
-                print(f"Pre Crouch Time: {preCrouchTimingLength}\n")
-
-                time.sleep(sleepTime - loopTime)  # 200 us ### IDEA: Link sleep time to dt in LERP of crouchingMechanism
+                time.sleep(max(0.0, sleepTime - loopTime))
+                #time.sleep(sleepTime - loopTime)  # 200 us ### IDEA: Link sleep time to dt in LERP of crouchingMechanism
 
 except KeyboardInterrupt:
         print("\nLoop stopped by user. Saving figure...")
         try:
-                plotFigure(timeSteps,hipOutputAngles,kneeOutputAngles,hipCommandAngles,kneeCommandAngles, crouchDuration, kpOutHipMoving, kdOutHipMoving)
+                plotFigure(timeSteps,hipOutputAngles,kneeOutputAngles,hipCommandAngles,kneeCommandAngles, crouchTime, kpOutHip, kdOutHip)
         except Exception as e:
                 print(f"Error encountered while saving figure: {e}")
         finally:

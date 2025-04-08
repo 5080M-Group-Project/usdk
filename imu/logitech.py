a97dfa2cacbc2ede59ffe11c1e98fa4d41a3453b
lucas0017 <lucas.dhesi@gmail.com>
2025-04-08 17:52:10 +0100

    Update logitech.py

--- imu/logitech.py
+++ imu/logitech.py
@@ -19,14 +19,15 @@ print(f"Connected to: {joystick.get_name()}")
 
 # Tracking button states
 prev_axis1 = -0.00
+prev_axis0 = -0.00
 #prev_button1 = 0
 kd = 1.0  # Example starting value
-
+kp = 1.0
 while True:
     pygame.event.pump()  # Process events
 
     # Read joystick inputs
-    axis_0 = joystick.get_axis(0)  # Left stick X-axis
+    axis_0 = round(joystick.get_axis(0),2)  # Left stick X-axis
     axis_1 = round(joystick.get_axis(1),2)  # Left stick Y-axis
     axis_2 = joystick.get_axis(2)  # Right stick X-axis
     axis_3 = joystick.get_axis(3)  # Right stick Y-axis
@@ -43,11 +44,19 @@ while True:
         kd -= 0.1
         print(f"dpad down pressed, kd decreased to {kd:.1f}")
 
+    if axis_0 == -1.00 and prev_axis0 == -0.00:
+        kp += 0.1
+        print(f"dpad up pressed, kd increased to {kp:.1f}")
+
+        # Detect single press event for button1 (decrement kd)
+    if axis_0 == 1.00 and prev_axis0 == -0.00:
+        kp -= 0.1
+        print(f"dpad down pressed, kd decreased to {kp:.1f}")
     # Update previous button states
     prev_axis1 = axis_1
     #prev_button1 = button1
 
     # Print joystick values
     print(
-        f"Left Stick: ({axis_0:.2f}, {axis_1:.2f}) | Right Stick: ({axis_2:.2f}, {axis_3:.2f}) | Button0: {button0}, Button1: {button1} | kd: {kd}")
+        f"Left Stick: ({axis_0:.2f}, {axis_1:.2f}) | Right Stick: ({axis_2:.2f}, {axis_3:.2f}) | Button0: {button0}, Button1: {button1} | kd: {kd:.1f}, kp: {kp:.1f}")
 

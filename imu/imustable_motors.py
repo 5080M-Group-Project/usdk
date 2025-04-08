e77948ce4c9f87d68c02146e42d235f28aa2c9f2
lucas0017 <lucas.dhesi@gmail.com>
2025-04-08 15:10:59 +0100

    Update imustable_motors.py

--- imu/imustable_motors.py
+++ imu/imustable_motors.py
@@ -19,7 +19,7 @@ data.motorType = MotorType.A1
 cmd.mode = queryMotorMode(MotorType.A1, MotorMode.FOC)
 
 # Gain tuning
-kpOutWheel, kdOutWheel = 10.0, 1
+kpOutWheel, kdOutWheel = 15.0, 1
 kpRotorWheel, kdRotorWheel = getRotorGains(kpOutWheel, kdOutWheel)
 
 # --- Setup IMU ---

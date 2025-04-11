915b6f226b181b2e129388a6affd6a8f926b003f
pm20ag <pm20ag@leeds.ac.uk>
2025-04-11 15:11:15 +0100

    Update AngleReadingTest.py

--- Test/AngleReadingTest.py
+++ Test/AngleReadingTest.py
@@ -49,7 +49,7 @@ try:
             data.motorType = MotorType.A1
             cmd.motorType = MotorType.A1
             cmd.mode = queryMotorMode(MotorType.A1, MotorMode.FOC)
-            cmd.id = 0
+            cmd.id = 1
             cmd.q = 0.0
             cmd.dq = 0.0  # 6.28*queryGearRatio(MotorType.A1)
             cmd.kp = 0.0

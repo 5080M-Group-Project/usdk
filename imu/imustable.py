e733c1a67b8d735014516bfd97c553bf1f392853
lucas0017 <lucas.dhesi@gmail.com>
2025-04-25 14:40:49 +0100

    Update imustable.py

--- imu/imustable.py
+++ imu/imustable.py
@@ -5,14 +5,16 @@ import adafruit_bno055
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
-sensor = adafruit_bno055.BNO055_I2C(i2c)
+imu = adafruit_bno055.BNO055_I2C(i2c)
 
 last_val = 0xFFFF
 
 
 while True:
-    pitch = sensor.euler[2]
-    pitchrate = sensor.gyro[2]
+    euler = imu.euler
+    gyro = imu.gyro
+    pitch = (euler[2]) if euler else None
+    pitchrate = (gyro[2]) if gyro else None 
     print(f"Gyroscope (rad/sec): {pitchrate:.2f}")
     print(f"pitch angle: {pitch:.2f}")
 

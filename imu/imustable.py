cc4a2c7c6ebd84212f83c4e8df482f65ac119912
lucas0017 <lucas.dhesi@gmail.com>
2025-04-25 14:34:04 +0100

    Update imustable.py

--- imu/imustable.py
+++ imu/imustable.py
@@ -9,8 +9,8 @@ sensor = adafruit_bno055.BNO055_I2C(i2c)
 
 last_val = 0xFFFF
 
-pitch = sensor.euler[2]
-pitchrate = sensor.gyro[2]
+pitch = format(sensor.euler[2])
+pitchrate = format(sensor.gyro[2])
 while True:
 
     print(f"Gyroscope (rad/sec): {pitchrate:.4f}")

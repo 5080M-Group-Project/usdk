4f2d59328acc0af34c480d65310dcd18d27c302c
lucas0017 <lucas.dhesi@gmail.com>
2025-04-28 16:23:27 +0100

    Update legbot.py

--- Test/legbot.py
+++ Test/legbot.py
@@ -68,7 +68,7 @@ wheel_radius = 0.05
 body_mass = 8.79
 robot_height = 0.175
 g = 9.81
-max_wheel_speed = 54  # rad/s
+max_wheel_speed = 20  # rad/s
 
 A = np.array([[0, 1, 0, 0],
               [0, 0, g / robot_height, 0],

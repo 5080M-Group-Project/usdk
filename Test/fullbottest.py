821847ab6e9035f7f9c9353d2ce9f8bee3cfa483
lucas0017 <lucas.dhesi@gmail.com>
2025-04-28 14:14:37 +0100

    Update fullbottest.py

--- Test/fullbottest.py
+++ Test/fullbottest.py
@@ -1,5 +1,7 @@
 import time
 import sys
+import board
+import adafruit_bno055
 
 sys.path.append('../lib')
 from unitree_actuator_sdk import *
@@ -32,7 +34,10 @@ cmd.motorType = MotorType.A1
 data.motorType = MotorType.A1
 cmd.mode = queryMotorMode(MotorType.A1, MotorMode.FOC)
 
-
+# --- IMU Setup ---
+i2c = board.I2C()
+imu = adafruit_bno055.BNO055_I2C(i2c)
+pitch = 0
 # --- Main Loop ---
 try:
     while True:
@@ -45,8 +50,8 @@ try:
         cmd.q = hip_angle_usb1  # Command hip angle in radians
         left.sendRecv(cmd, data)  # Send command to USB1 hip motor
         angle = data.q
-        print(f"Raw left hip angle reading (radx9):  {angle} ")
-        print(f"USB1 - Hip Commanded Angle (rad): {hip_angle_usb1}")
+        #print(f"Raw left hip angle reading (radx9):  {angle} ")
+        #print(f"USB1 - Hip Commanded Angle (rad): {hip_angle_usb1}")
 
         cmd.id = 1  # Knee motor ID for USB1
         cmd.dq = 0
@@ -55,8 +60,8 @@ try:
         cmd.q = knee_angle_usb1  # Command knee angle in radians
         left.sendRecv(cmd, data)  # Send command to USB1 knee motor
         angle = data.q
-        print(f"Raw left knee angle reading (radx9):  {angle} ")
-        print(f"USB1 - Knee Commanded Angle (rad): {knee_angle_usb1}")
+        #print(f"Raw left knee angle reading (radx9):  {angle} ")
+        #print(f"USB1 - Knee Commanded Angle (rad): {knee_angle_usb1}")
 
         # Send commands for USB0 (hip and knee motors)
         cmd.id = 0  # Hip motor ID for USB0
@@ -65,8 +70,8 @@ try:
         cmd.q = hip_angle_usb0  # Command hip angle in radians
         right.sendRecv(cmd, data)  # Send command to USB0 hip motor
         angle = data.q
-        print(f"Raw right hip angle reading (radx9):  {angle} ")
-        print(f"USB0 - Hip Commanded Angle (rad): {hip_angle_usb0}")
+        #print(f"Raw right hip angle reading (radx9):  {angle} ")
+        #print(f"USB0 - Hip Commanded Angle (rad): {hip_angle_usb0}")
 
         cmd.id = 1  # Knee motor ID for USB0
         cmd.kp = kpRotorWheel
@@ -74,8 +79,8 @@ try:
         cmd.q = knee_angle_usb0  # Command knee angle in radians
         right.sendRecv(cmd, data)  # Send command to USB0 knee motor
         angle = data.q
-        print(f"Raw right knee angle reading (radx9):  {angle} ")
-        print(f"USB0 - Knee Commanded Angle (rad): {knee_angle_usb0}")
+        #print(f"Raw right knee angle reading (radx9):  {angle} ")
+        #print(f"USB0 - Knee Commanded Angle (rad): {knee_angle_usb0}")
 
         #CONTROL LOOP
 
@@ -93,7 +98,14 @@ try:
         left.sendRecv(cmd, data)  # Send command to USB0 knee motor
         angle = data.q
 
-
+        if euler[2] is not None:
+            if euler[2] < 0:
+                pitch = -180 - euler[2]
+            else:
+                pitch = 180 - euler[2]
+        else:
+            pitch = pitch
+        print(f"Pitch:  {pitch} ")
 
         # Wait a little before sending the next command
         time.sleep(0.1)
